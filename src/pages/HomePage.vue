d88b84ccba493b709353b168b5b7a266b43f19a2
Jorge Muniz <jorgemp221@msn.com>
2024-05-22 20:26:53 -0600

    Add apiServices.ts to fetch posts from Reddit API

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,4 +4,20 @@
     </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { fetchPosts } from '../services/apiServices';
+onMounted(() => {
+    loadPosts()
+})
+
+const loadPosts = async () => {
+    try {
+        const posts = await fetchPosts('computerscience');
+        console.log(posts);
+    } catch (error) {
+
+    }
+}
+
+</script>
---
+++ src/services/apiServices.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+//TypeScript interfaces for the Reddit API response
+export interface RedditPost {
+  id: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  ups: number;
+  url: string;
+  created_utc: number;
+}
+
+//Fetch post from a subreddit
+export const fetchPosts = async (subreddit: string): Promise<RedditPost[]> => {
+  try {
+    const response = await axios.get(`https://www.reddit.com/r/${subreddit}.json`);
+    const posts = response?.data?.data?.children.map((child: any) => ({
+      id: child.data.id,
+      title: child.data.title,
+      author: child.data.author,
+      num_comments: child.data.num_comments,
+      ups: child.data.ups,
+      url: child.data.url,
+      created_utc: child.data.created_utc,
+    }));
+    return posts;
+  } catch (error) {
+    throw error;
+  }
+};
